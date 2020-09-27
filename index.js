17b3b019f764e581242efc32364a72c202013300
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-27 15:33:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const prefix = "="
+const prefix = "send"
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-});
+    bot.user.setActivity('Now Accepting Staff Applications!', { 
+        type: "PLAYING",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    });
+
+}
+)
 
 bot.on('message', message => {
 
@@ -19,16 +25,44 @@ bot.on('message', message => {
 
     if (com === `${prefix}dm`) {
         if (!message.member.permissions.has("MANAGE_MESSAGES"))
-            return message.channel.send(`You don't have permission to use this command.`);
+            const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#fb1309')
+		.setTitle('ERROR')
+		.setAuthor('Among Sus Applications', 'https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setDescription("You don't have permission to use this command!")
+		.setThumbnail('https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+    }
         let user =
             message.mentions.members.first() ||
             message.guild.members.cache.get(args[1]);
         if (!user)
-            return message.channel.send(
-                `You did not mention a user`
-            );
+            const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#fb1309')
+		.setTitle('ERROR')
+		.setAuthor('Among Sus Applications', 'https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setDescription("You didn't mention a user!")
+		.setThumbnail('https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+    }
         if (!args.slice(2).join(" "))
-            return message.channel.send("You did not specify your message");
+            const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#fb1309')
+		.setTitle('ERROR')
+		.setAuthor('Among Sus Applications', 'https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setDescription("You did not specify your message!")
+		.setThumbnail('https://media.discordapp.net/attachments/759887956328316940/759889650432606228/sabotage.png?width=401&height=401')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+    }
         user.user
             .send(args.slice(1).join(" "))
             .catch(() => message.channel.send("That user could not be DMed!"))
